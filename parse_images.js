4ca415f1657f958f141b2f6747e5935a058363d1
Ashish Gandhi <ag@ashonomics.com>
2012-01-12 10:04:42 -0500

    Added another test link
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- parse_images.js
+++ parse_images.js
@@ -1,8 +1,10 @@
 var jsdom = require("jsdom");
 var im = require('imagemagick');
 
-var link = "http://www.shopbop.com/rouched-pencil-skirt-james-perse/vp/v=1/845524441916289.htm?folderID=2534374302023782&extid=affprg-4022050-JPERS4008112588&colorId=12588";
+var link = 'http://www.pinkmascara.com/anna-kosturova-marilyn-dress-as-seen-on-miranda-kerr-2';
+// var link = "http://www.shopbop.com/rouched-pencil-skirt-james-perse/vp/v=1/845524441916289.htm?folderID=2534374302023782&extid=affprg-4022050-JPERS4008112588&colorId=12588";
 // var link = "http://www.google.com/";
+
 var biggestArea = -1;
 var biggestImage = 'http://stage.assets.gotryiton.s3.amazonaws.com/outfits/de69108096c07298adb6c6ac261cf40a_137_182.jpg';
 
@@ -16,7 +18,7 @@ function(errors, window) {
     if(imageUrl == '') return true;
     if(imageUrl.slice(0, 4) != 'http') imageUrl = link.split('/')[0] + '//' + link.split('/')[2] + imageUrl;
     im.identify(imageUrl, function(err, features){
-      if (err) return; // throw err;
+      if (err) return true; // throw err;
       var area = features['height'] * features['width'];
       if (area > biggestArea) {
         biggestArea = area;
