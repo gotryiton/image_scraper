78ddcfbb2208487486de2e0a993279343e7cb650
Ashish Gandhi <ag@ashonomics.com>
2012-01-11 23:11:52 -0500

    Fixed bug to correctly detect absolute URLs
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- parse_images.js
+++ parse_images.js
@@ -12,9 +12,8 @@ function(errors, window) {
   console.log("The page has", window.$("img").length, "images");
   window.$("img").each(function() {
     var img_url = window.$(this).attr("src");
-    if(img_url == '' || img_url == '/') return true;
-    // bug
-    if(img_url.split('/')[0] != 'http') img_url = link.split('/')[0] + '//' + link.split('/')[2] + img_url;
+    if(img_url == '') return true;
+    if(img_url.slice(0, 4) != 'http') img_url = link.split('/')[0] + '//' + link.split('/')[2] + img_url;
     console.log(img_url);
     im.identify(img_url, function(err, features){
       if (err) return; // throw err;
