5e772af807dea6d687ffc793c3d499434526c2c2
Ashish Gandhi <ag@ashonomics.com>
2012-01-12 17:28:24 -0500

    Added restify
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- parse_images.js
+++ parse_images.js
@@ -10,33 +10,35 @@ var jquery = fs.readFileSync("./jquery-1.7.1.min.js").toString();
 // var link = 'http://www.80spurple.com/shop/product/136846/5133/purple-label-womens-ashley-button-down-sleeveless-top-emerald';
 // var link = 'http://www.google.com/';
 // var link = 'http://www.shopbop.com/pencil-skirt-blank-denim/vp/v=1/845524441925870.htm?folderID=2534374302153272&extid=affprg-4022050-BLANK4006113022&colorId=13022';
-var link = 'http://www.flickr.com/explore/';
+var url = 'http://www.flickr.com/explore/';
 
-var biggestArea = -1;
-var biggestImage = 'http://stage.assets.gotryiton.s3.amazonaws.com/outfits/de69108096c07298adb6c6ac261cf40a_137_182.jpg';
-
-jsdom.env({
-  html: link,
-  src: [
-    jquery
-  ],
-  done: function(errors, window) {
-    if (errors) throw errors;
-    console.log("Number of images on page", window.$("img").length);
-    window.$("img").each(function() {
-      var imageUrl = window.$(this).attr("src");
-      if(imageUrl == '') return true;
-      if(imageUrl.slice(0, 4) != 'http') imageUrl = link.split('/')[0] + '//' + link.split('/')[2] + imageUrl;
-      im.identify(imageUrl, function(err, features){
-        if (err) return true; // throw err;
-        var area = features['height'] * features['width'];
-        if (area >= biggestArea) {
-          biggestArea = area;
-          biggestImage = imageUrl;
-        }
-        console.log("Biggest image is", biggestImage, "of area", biggestArea);
+function getLargestImage(link) {
+  var biggestArea = -1;
+  var biggestImage = 'http://stage.assets.gotryiton.s3.amazonaws.com/outfits/de69108096c07298adb6c6ac261cf40a_137_182.jpg';
+  jsdom.env({
+    html: link,
+    src: [
+      jquery
+    ],
+    done: function(errors, window) {
+      if (errors) throw errors;
+      // console.log("Number of images on page", window.$("img").length);
+      window.$("img").each(function() {
+        var imageUrl = window.$(this).attr("src");
+        if(imageUrl == '') return true;
+        if(imageUrl.slice(0, 4) != 'http') imageUrl = link.split('/')[0] + '//' + link.split('/')[2] + imageUrl;
+        im.identify(imageUrl, function(err, features){
+          if (err) return true; // throw err;
+          var area = features['height'] * features['width'];
+          if (area >= biggestArea) {
+            biggestArea = area;
+            biggestImage = imageUrl;
+          }
+          // console.log("Biggest image is", biggestImage, "of area", biggestArea);
+        });
       });
-    });
-  }
-});
+    }
+  });
+  return biggestImage;
+}
     
