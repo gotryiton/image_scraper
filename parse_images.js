c6ec8e8cad7e186fb4d7d50ecc6edeaf8eced1d0
Ashish Gandhi <ag@ashonomics.com>
2012-01-11 20:22:24 -0500

    Parse images using imagemagick
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- parse_images.js
+++ parse_images.js
@@ -1,16 +1,24 @@
 var jsdom = require("jsdom");
-var link_ = "http://www.shopbop.com/rouched-pencil-skirt-james-perse/vp/v=1/845524441916289.htm?folderID=2534374302023782&extid=affprg-4022050-JPERS4008112588&colorId=12588";
-var link = "http://www.google.com/";
+var im = require('imagemagick');
+
+var link = "http://www.shopbop.com/rouched-pencil-skirt-james-perse/vp/v=1/845524441916289.htm?folderID=2534374302023782&extid=affprg-4022050-JPERS4008112588&colorId=12588";
+// var link = "http://www.google.com/";
 
 jsdom.env(link, [
-  'http://code.jquery.com/jquery-1.5.min.js'
+  'http://code.jquery.com/jquery-1.7.min.js'
 ],
 function(errors, window) {
+  if (errors) throw errors;
   // console.log("The page has", window.$("img").length, "images");
   window.$("img").each(function() {
     var img_url = window.$(this).attr("src");
+    if(img_url == '' || img_url == '/') return true;
     if(img_url.split('/')[0] != 'http') img_url = link.split('/')[0] + '//' + link.split('/')[2] + img_url;
     console.log(img_url);
-    console.log(window.$(this).height());
+    im.identify(img_url, function(err, features){
+      if (err) return; // throw err;
+      console.log("Area is", features['height'] * features['width']);
+      console.log("");
+    });
   });
 });
