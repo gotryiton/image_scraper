0dae883352de8f039a86f9d48f89f58c3618906e
Ashish Gandhi <ag@ashonomics.com>
2012-01-13 13:34:47 -0500

    The server works now
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- parse_images.js
+++ parse_images.js
@@ -4,40 +4,45 @@ var fs = require('fs');
 var jqueryString = fs.readFileSync("./jquery-1.7.1.min.js").toString();
 var restify = require('restify');
 
-// var url = 'http://www.pinkmascara.com/anna-kosturova-marilyn-dress-as-seen-on-miranda-kerr-2';
-// var url = "http://www.shopbop.com/rouched-pencil-skirt-james-perse/vp/v=1/845524441916289.htm?folderID=2534374302023782&extid=affprg-4022050-JPERS4008112588&colorId=12588";
-// var url = "http://shop.nordstrom.com/s/donna-morgan-belted-chiffon-dress/3172484?cm_ite=donna_morgan_belted_chiffon_dress:259207_7&cm_pla=dresses:women:dress&cm_ven=Linkshare&siteid=xC_K04x1nMI-UkWMJmyPLfjRCCVlsUYKFw&url=http://shop.nordstrom.com/S/3172484?cm_cat=datafeed";
-// var url = 'http://www.michaelstars.com/p-6463.php?=&utm_source=affiliatetraction&utm_medium=commissionjunction&utm_campaign=http://www.michaelstars.com/p-6463.php';
-// var url = 'http://www.80spurple.com/shop/product/136846/5133/purple-label-womens-ashley-button-down-sleeveless-top-emerald';
-// var url = 'http://www.google.com/';
-var link = 'http://www.shopbop.com/pencil-skirt-blank-denim/vp/v=1/845524441925870.htm?folderID=2534374302153272&extid=affprg-4022050-BLANK4006113022&colorId=13022';
-// var url = 'http://www.flickr.com/explore/';
-
 var server = restify.createServer();
 
-server.get('/largest_image', function(req, res) {
+server.post('/largest_image', function(req, res) {
   var url = req.params.url;
-  
   var biggestArea = -1;
   var biggestImage = 'http://stage.assets.gotryiton.s3.amazonaws.com/outfits/de69108096c07298adb6c6ac261cf40a_137_182.jpg';
   var count = 0;
   
   jsdom.env({
-    html: link,
+    html: url,
     src: [
       jqueryString
     ],
     done: function(errors, window) {
-      if (errors) throw errors;
+      // on error send back the default image
+      if (errors) {
+        res.send(200, {
+          url: biggestImage
+        });
+        return true; //throw errors;
+      }
+      
+      // set the count to know when all images processed
       count = window.$("img").length;
-      console.log("Number of images on page", window.$("img").length);
+      
+      // iterate through all images on the page
       window.$("img").each(function() {
         var imageUrl = window.$(this).attr("src");
+        
+        // don't waste time requesting these, need to count them though
         if(imageUrl == '') {
           count--;
           return true;
         }
+        
+        // convert URLs form relative to absolute
         if(imageUrl.slice(0, 4) != 'http') imageUrl = link.split('/')[0] + '//' + link.split('/')[2] + imageUrl;
+        
+        // let imagemagick fetch and analyze the images in async
         im.identify(imageUrl, function(err, features){
           count--;
           if (err) return true; // throw err;
@@ -46,6 +51,8 @@ server.get('/largest_image', function(req, res) {
             biggestArea = area;
             biggestImage = imageUrl;
           }
+          
+          // when all images are done processing, return the biggest
           if(!count) {
             res.send(200, {
               url: biggestImage
