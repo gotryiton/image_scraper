cf10c7e8cc466bb7279348db708f5084d76a5a09
Scott Bader <sbader2@gmail.com>
2012-01-23 19:50:07 -0500

    setting filenames to use correct directory

--- test/test-scraper.js
+++ test/test-scraper.js
@@ -1,8 +1,8 @@
 require('../scraper.js');
 var fs = require('fs');
 
-var metaPage = fs.readFileSync('./test_pages/biggest-image-meta-tags-page.html').toString();
-var ogPage = fs.readFileSync('./test_pages/open-graph-page.html').toString();
+var metaPage = fs.readFileSync(__dirname+'/test_helpers/biggest-image-meta-tags-page.html').toString();
+var ogPage = fs.readFileSync(__dirname+'/test_helpers/open-graph-page.html').toString();
 
 exports['title'] = function(test) {
   var sc = new Scraper('http://www.gotryiton.com/');
@@ -28,7 +28,7 @@ exports['og-image'] = function(test) {
 
 exports['image-area'] = function(test) {
   var sc = new Scraper('http://www.gotryiton.com/');
-  sc.getImageArea('fire-lizard.jpg', function(image, area){
+  sc.getImageArea(__dirname+'/test_helpers/fire-lizard.jpg', function(image, area){
     test.equal(area, 127200);
     test.done();
   });
