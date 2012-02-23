a87f04b333b87e3868b27167989667fe666d41e7
Ashish Gandhi <ag@ashonomics.com>
2012-02-23 12:15:29 -0500

    Description tests added (was a typo what was breaking it)
    Signed-off-by: Ashish Gandhi <ag@ashonomics.com>

--- test/test-extenal-sites.js
+++ test/test-extenal-sites.js
@@ -10,10 +10,10 @@ var sites = [
 
 var expectedResponses = [
   {'title': 'ASOS Block Colour Cagoule', 'image': 'http://images.asos.com/inv/media/7/7/8/9/1919877/blue/image1xl.jpg', 'description': null},
-  {'title': '7 For All Mankind Cropped Skinny Jeans | SHOPBOP', 'image': 'http://g-ecx.images-amazon.com/images/G/01/Shopbop/p/pcs/products/seven/seven4015611318/seven4015611318_p4_1-0_254x500.jpg', 'descripiton': '7 For All Mankind Cropped Skinny Jeans at SHOPBOP.COM - FASTEST FREE SHIPPING WORLDWIDE. Buy 7 For All Mankind Online'},
-  {'title': 'FADED GREY TROUSERS - Jeans - Woman - ZARA United States', 'image': 'http://static.zara.net/photos//2012/V/0/1/p/6045/042/804/6045042804_1_1_3.jpg?timestamp=1328289461558', 'descripiton': 'FADED GREY TROUSERS'},
-  {'title': 'DKNY \'Sunny Escape\' Leggings | Nordstrom', 'image': 'http://g-lvl3.nordstromimage.com/ImageGallery/store/product/Medium/9/_6821389.jpg', 'descripiton': 'DKNY \'Sunny Escape\' Leggings | Nordstrom'},
-  {'title': 'Large Gold Hammered Teardrop Earrings by Kenneth Jay Lane | Charm & Chain', 'image': 'http://cdn.shopify.com/s/files/1/0019/0222/products/IMG_0126_medium.jpg?106717', 'descripiton': 'Large Gold Hammered Teardrop Earrings: Thereâ€™s...'}
+  {'title': '7 For All Mankind Cropped Skinny Jeans | SHOPBOP', 'image': 'http://g-ecx.images-amazon.com/images/G/01/Shopbop/p/pcs/products/seven/seven4015611318/seven4015611318_p4_1-0_254x500.jpg', 'description': '7 For All Mankind Cropped Skinny Jeans at SHOPBOP.COM - FASTEST FREE SHIPPING WORLDWIDE. Buy 7 For All Mankind Online'},
+  {'title': 'FADED GREY TROUSERS - Jeans - Woman - ZARA United States', 'image': 'http://static.zara.net/photos//2012/V/0/1/p/6045/042/804/6045042804_1_1_3.jpg?timestamp=1328289461558', 'description': 'FADED GREY TROUSERS'},
+  {'title': 'DKNY \'Sunny Escape\' Leggings | Nordstrom', 'image': 'http://g-lvl3.nordstromimage.com/ImageGallery/store/product/Medium/9/_6821389.jpg', 'description': 'DKNY \'Sunny Escape\' Leggings | Nordstrom'},
+  {'title': 'Large Gold Hammered Teardrop Earrings by Kenneth Jay Lane | Charm & Chain', 'image': 'http://cdn.shopify.com/s/files/1/0019/0222/products/IMG_0126_medium.jpg?106717', 'description': 'Large Gold Hammered Teardrop Earrings: There’s...'}
 ];
 
 exports['test-mobile-sites'] = function(test) {
@@ -24,7 +24,7 @@ exports['test-mobile-sites'] = function(test) {
       var expectedResponse = expectedResponses[index];
       test.equals(data['title'], expectedResponse['title']);
       test.equals(data['image'], expectedResponse['image']);
-      // test.equals(data['descripiton'], expectedResponse['descripiton']);
+      test.equals(data['description'], expectedResponse['description']);
       count--;
       if (count == 0) {
         test.done();
